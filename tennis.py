4546d12647675536e52dafd3035fd4abf6212ba5
marty <MatthieuArchez>
2019-01-09 20:32:42 +0100

    Testing and coding of the Score class - handling games, sets and tie break
    Class Score is a data representation of the tennis score board, including:
    inc_points : handling basic score game counting, calling inc_tie_break in case of tie break situation
    inc_games : handling games' increments, end of set handling and tie break detection
    inc_sets : handling sets increments and end game conditions
    inc_tie_break : tie break special kind of counting (end of TB whenever a minimum of 7 pts is attained with 2 points difference
    
    The package further includes a game loop for client testing purpose !

--- tennis.py
+++ tennis.py
@@ -1,31 +1,142 @@
+#!/dlip_ref/s_dlip/Tools/LDT_LAB/linux64/python/python-3.6.3/bin/python
 """
     Tennis module
 """
+NB_WINNING_SETS = 3
 
-def inc_points(pts_p1, pts_p2, winner_id):
+class Score:
     """
-        Tennis board computing game points
+        Data representation for tennis score
     """
-    pts_winner, pts_loser = (pts_p1, pts_p2) if winner_id == 1 else (pts_p2, pts_p1)
+    def __init__(self, points=[0, 0], games=[[0, 0]], tb_points=[0, 0], sets=[0, 0]):
+        self.games = games
+        self.points = points
+        self.tb_points = tb_points
+        self.sets = sets
+        self.tie_break = self._is_tie_break()
 
-    if pts_winner == 0:
-        pts_winner = 15
-    elif pts_winner == 15:
-        pts_winner = 30
-    elif pts_winner == 30:
-        pts_winner = 40
-    elif pts_winner == 40:
-        if pts_loser == 40:
-            pts_winner = "Ad."
-            pts_loser = " "
+    @staticmethod
+    def _get_index(winner_id):
+        return (0, 1) if winner_id == 1 else (1, 0)
+
+    def _is_tie_break(self):
+        return True if self.games[-1] == [6, 6] else False
+
+    def __str__(self):
+        sp1, sp2 = self.sets
+        score_str = "\nSETS : " + str(sp1) + " / " + str(sp2) + "\nGAMES : "
+        for game in self.games:
+            sp1, sp2 = game
+            score_str += str(sp1) + "/" + str(sp2) + "  "
+
+        if self.tie_break:
+            sp1, sp2 = self.tb_points
+            score_str += "\nTIE BREAK : " + str(sp1) + " / " + str(sp2) + "\n\n"
         else:
-            pts_winner = 0
-            pts_loser = 0
-    elif pts_winner == "Ad.":
-        pts_winner = 0
-        pts_loser = 0
-    else:
-        pts_winner = 40
-        pts_loser = 40
+            sp1, sp2 = self.points
+            score_str += "\n" + str(sp1) + " / " + str(sp2) + "\n\n"
 
-    return (pts_winner, pts_loser) if winner_id == 1 else (pts_loser, pts_winner)
+        return score_str
+
+    def inc_sets(self, winner_id):
+        """
+            Increments sets counter
+            return updated sets score, True if Match is over, False otherwise
+        """
+        winner_index, _unused_loser_index = Score._get_index(winner_id)
+
+        self.sets[winner_index] += 1
+
+        return True if self.sets[winner_index] == NB_WINNING_SETS else False
+
+    def inc_games(self, winner_id):
+        """
+            Tennis board computing games
+            return the updated point score, and True if the set is over, False otherwise
+        """
+        winner_index, loser_index = Score._get_index(winner_id)
+
+        self.games[-1][winner_index] += 1
+        self.tie_break = Score._is_tie_break(self)
+
+        return True if ((self.games[-1][winner_index] == 6 and\
+                         self.games[-1][loser_index] < 5) or\
+                        (self.games[-1][winner_index] == 7 and\
+                         self.games[-1][loser_index] in (5, 6)))\
+                    else False
+
+    def inc_tie_break(self, winner_id):
+        """
+            Tennis tie break counter
+        """
+        winner_index, loser_index = Score._get_index(winner_id)
+
+        self.tb_points[winner_index] += 1
+
+        return True if self.tb_points[winner_index] >= 7 and\
+                       self.tb_points[winner_index] - self.tb_points[loser_index] >= 2 else False
+
+
+    def inc_points(self, winner_id):
+        """
+            Tennis board computing game points
+            return the updated point score, and True if the point is over, False otherwise
+        """
+        game_end = False
+
+        if self.tie_break:
+            game_end = self.inc_tie_break(winner_id)
+        else:
+            winner_index, loser_index = Score._get_index(winner_id)
+
+            if self.points[winner_index] == 0:
+                self.points[winner_index] = 15
+            elif self.points[winner_index] == 15:
+                self.points[winner_index] = 30
+            elif self.points[winner_index] == 30:
+                self.points[winner_index] = 40
+            elif self.points[winner_index] == 40:
+                if self.points[loser_index] == 40:
+                    self.points[winner_index] = "Ad."
+                    self.points[loser_index] = " "
+                else:
+                    game_end = True
+                    self.points[winner_index] = 0
+                    self.points[loser_index] = 0
+            elif self.points[winner_index] == "Ad.":
+                game_end = True
+                self.points[winner_index] = 0
+                self.points[loser_index] = 0
+            else:
+                self.points[winner_index] = 40
+                self.points[loser_index] = 40
+
+        return game_end
+
+def game_loop(score=Score()):
+    """
+        Example of game loop using the score class
+    """
+    def get_winner_id():
+        """
+            Getting winner_id from user input
+        """
+        winner_id = ""
+        print("Who win ? (1/2)")
+
+        while winner_id not in ("1", "2"):
+            winner_id = input()
+
+        return int(winner_id)
+
+    match_over = False
+
+    while not match_over:
+        print(score)
+
+        winner_id = get_winner_id()
+
+        if score.inc_points(winner_id):
+            if score.inc_games(winner_id):
+                if score.inc_sets(winner_id):
+                    match_over = True
--- test.py
+++ test.py
@@ -1,42 +1,162 @@
+#!/dlip_ref/s_dlip/Tools/LDT_LAB/linux64/python/python-3.6.3/bin/python
 """
     Tennis Test Module
 
 """
 
 import unittest
-from .tennis import inc_points
+from tennis import Score
+
 
 class TennisTest(unittest.TestCase):
     """
         Unittest implementation for Tennis
     """
+
     def test_inc_points_no_advantage(self):
         """
             Basic counting
         """
-        self.assertEqual(inc_points(0, 0, 1), (15, 0))
-        self.assertEqual(inc_points(15, 0, 1), (30, 0))
-        self.assertEqual(inc_points(30, 30, 1), (40, 30))
-        self.assertEqual(inc_points(0, 30, 1), (15, 30))
+        score = Score()
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [15, 0])
 
-        self.assertEqual(inc_points(30, 15, 2), (30, 30))
-        self.assertEqual(inc_points(0, 30, 2), (0, 40))
+        score = Score([15, 0])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [30, 0])
+
+        score = Score([30, 30])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [40, 30])
+
+        score = Score([0, 30])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [15, 30])
+
+        score = Score([30, 15])
+        self.assertEqual(score.inc_points(2), False)
+        self.assertEqual(score.points, [30, 30])
+
+        score = Score([0, 30])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [15, 30])
+
+        score = Score([0, 30])
+        self.assertEqual(score.inc_points(2), False)
+        self.assertEqual(score.points, [0, 40])
 
     def test_inc_points_advantages(self):
         """
             Advantages handling
         """
-        self.assertEqual(inc_points(40, 40, 2), (" ", "Ad."))
-        self.assertEqual(inc_points(40, 40, 1), ("Ad.", " "))
-        self.assertEqual(inc_points("Ad.", " ", 2), (40, 40))
-        self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40))
+        score = Score([40, 40])
+        self.assertEqual(score.inc_points(2), False)
+        self.assertEqual(score.points, [" ", "Ad."])
+
+        score = Score([40, 40])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, ["Ad.", " "])
+
+        score = Score(["Ad.", " "])
+        self.assertEqual(score.inc_points(2), False)
+        self.assertEqual(score.points, [40, 40])
+
+        score = Score([" ", "Ad."])
+        self.assertEqual(score.inc_points(1), False)
+        self.assertEqual(score.points, [40, 40])
 
     def test_inc_points_end_game(self):
         """
             End game conditions
         """
-        self.assertEqual(inc_points(40, 30, 1), (0, 0))
-        self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0))
+        score = Score([40, 30])
+        self.assertEqual(score.inc_points(1), True)
+        self.assertEqual(score.points, [0, 0])
+
+        score = Score([" ", "Ad."])
+        self.assertEqual(score.inc_points(2), True)
+        self.assertEqual(score.points, [0, 0])
+
+    def test_inc_game(self):
+        """
+            General case w/o tie break and set
+        """
+        score = Score(games=[[0, 0]])
+        self.assertEqual(score.inc_games(1), False)
+        self.assertEqual(score.games, [[1, 0]])
+
+        score = Score(games=[[0, 0]])
+        self.assertEqual(score.inc_games(2), False)
+        self.assertEqual(score.games, [[0, 1]])
+
+        score = Score(games=[[6, 4], [1, 0]])
+        self.assertEqual(score.inc_games(1), False)
+        self.assertEqual(score.games, [[6, 4], [2, 0]])
+
+        score = Score(games=[[4, 4]])
+        self.assertEqual(score.inc_games(1), False)
+        self.assertEqual(score.games, [[5, 4]])
+
+        score = Score(games=[[6, 4], [5, 5]])
+        self.assertEqual(score.inc_games(2), False)
+        self.assertEqual(score.games, [[6, 4], [5, 6]])
+
+    def test_inc_game_end(self):
+        """
+            Game ends
+        """
+        score = Score(games=[[5, 4]])
+        self.assertEqual(score.inc_games(1), True)
+        self.assertEqual(score.games, [[6, 4]])
+
+        score = Score(games=[[4, 5]])
+        self.assertEqual(score.inc_games(2), True)
+        self.assertEqual(score.games, [[4, 6]])
+
+        score = Score(games=[[6, 5]])
+        self.assertEqual(score.inc_games(1), True)
+        self.assertEqual(score.games, [[7, 5]])
+
+    def test_inc_game_tie_break(self):
+        """
+            Tie Break test
+        """
+        score = Score(games=[[6, 5]])
+        self.assertEqual(score.inc_games(2), False)
+        self.assertEqual(score.games, [[6, 6]])
+
+        score = Score(tb_points=[6, 6])
+        self.assertEqual(score.inc_tie_break(2), False)
+        self.assertEqual(score.tb_points, [6, 7])
+
+        score = Score(tb_points=[6, 0])
+        self.assertEqual(score.inc_tie_break(1), True)
+        self.assertEqual(score.tb_points, [7, 0])
+
+        score = Score(tb_points=[7, 6])
+        self.assertEqual(score.inc_tie_break(1), True)
+        self.assertEqual(score.tb_points, [8, 6])
+
+        score = Score(tb_points=[16, 15])
+        self.assertEqual(score.inc_tie_break(1), True)
+        self.assertEqual(score.tb_points, [17, 15])
+
+    def test_inc_sets(self):
+        """
+            Test increments sets
+        """
+        score = Score(sets=[2, 2])
+        self.assertEqual(score.inc_sets(1), True)
+        self.assertEqual(score.sets, [3, 2])
+
+        score = Score(sets=[0, 2])
+        self.assertEqual(score.inc_sets(2), True)
+        self.assertEqual(score.sets, [0, 3])
+
+        score = Score(sets=[0, 0])
+        self.assertEqual(score.inc_sets(1), False)
+        self.assertEqual(score.sets, [1, 0])
+
 
 if __name__ == "__main__":
     unittest.main()
