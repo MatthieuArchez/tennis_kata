cac4a75dba3b7f05e62e293427e49d99f9355bd6
marty <MatthieuArchez>
2019-01-09 20:21:29 +0100

    Pylint OK

--- test.py
+++ test.py
@@ -1,26 +1,42 @@
+"""
+    Tennis Test Module
+
+"""
+
 import unittest
-from tennis import inc_points
+from .tennis import inc_points
 
+class TennisTest(unittest.TestCase):
+    """
+        Unittest implementation for Tennis
+    """
+    def test_inc_points_no_advantage(self):
+        """
+            Basic counting
+        """
+        self.assertEqual(inc_points(0, 0, 1), (15, 0))
+        self.assertEqual(inc_points(15, 0, 1), (30, 0))
+        self.assertEqual(inc_points(30, 30, 1), (40, 30))
+        self.assertEqual(inc_points(0, 30, 1), (15, 30))
 
-class Tennis_Test(unittest.TestCase):
-	def test_inc_points_no_advantage(self):
-		self.assertEqual(inc_points(0, 0, 1), (15, 0))
-		self.assertEqual(inc_points(15, 0, 1), (30, 0))
-		self.assertEqual(inc_points(30, 30, 1), (40, 30))
-		self.assertEqual(inc_points(0, 30, 1), (15, 30))
+        self.assertEqual(inc_points(30, 15, 2), (30, 30))
+        self.assertEqual(inc_points(0, 30, 2), (0, 40))
 
-		self.assertEqual(inc_points(30, 15, 2), (30, 30))
-		self.assertEqual(inc_points(0, 30, 2), (0, 40))
+    def test_inc_points_advantages(self):
+        """
+            Advantages handling
+        """
+        self.assertEqual(inc_points(40, 40, 2), (" ", "Ad."))
+        self.assertEqual(inc_points(40, 40, 1), ("Ad.", " "))
+        self.assertEqual(inc_points("Ad.", " ", 2), (40, 40))
+        self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40))
 
-	def test_inc_points_advantages(self):
-		self.assertEqual(inc_points(40, 40, 2), (" ", "Ad."))
-		self.assertEqual(inc_points(40, 40, 1), ("Ad.", " "))
-		self.assertEqual(inc_points("Ad.", " ", 2), (40, 40))
-		self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40))
-
-	def test_inc_points_end_game(self):
-		self.assertEqual(inc_points(40, 30, 1), (0, 0))
-		self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0))
+    def test_inc_points_end_game(self):
+        """
+            End game conditions
+        """
+        self.assertEqual(inc_points(40, 30, 1), (0, 0))
+        self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0))
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
